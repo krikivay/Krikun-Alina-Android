a8bd469f05f2b3220940543b3f7eb448d6917793
krikivay <krikivay@gmail.com>
2020-03-09 22:32:31 +0200

    LR3

--- MyApplication2/MainActivity.java
+++ MyApplication2/MainActivity.java
@@ -7,6 +7,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -18,11 +21,17 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = (Button) findViewById(R.id.button);
+        btn = (Button) findViewById(R.id.bLogin);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                DatePicker datePicker = (DatePicker) findViewById(R.id.dpDate);
+                ArrayList<Integer> date = new ArrayList<>();
+                date.add(datePicker.getYear());
+                date.add(datePicker.getMonth() + 1);
+                date.add(datePicker.getDayOfMonth());
+                intent.putExtra("date", date);
                 startActivity(intent);
             }
         });
